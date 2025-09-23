5012795ef61ab335a929b8283a735998b49f18c8
miftahadzdzaudanislam <miftahdzaudan@gmail.com>
2025-09-23 13:42:30 +0700

    tugas 5 javascript

--- index.js
+++ index.js
@@ -9,31 +9,31 @@ let productList = [
 
 // Fungsi menambah produk dengan spread operator
 const addProduct = (id, name, price) => {
+    console.log(`\nMenambah produk ${name} dengan harga ${price}`);
     productList = [...productList, { id, name, price }];
 }
 
 // Fungsi menghapus produk dengan rest parameter
 const removeProduct = (...id) => {
+    console.log(`\nMenghapus produk pada id ${id}`);
     productList = productList.filter(product => !id.includes(product.id));
 }
 
 // Fungsi menampilkan produk dengan destructuring
 const viewProducts = () => {
+    console.log("Menampilkan semua produk pada");
     for (const { id, name, price } of productList) {
         console.log(`${id}. ${name} - Rp ${price}`);
     }
 }
 
 // Menampilkan semua produk
-console.log("Menampilkan semua produk:");
 viewProducts();
 
 // Menambah produk baru
-console.log("\nMenambah produk baru:");
 addProduct(6, "Mouse", 90000);
 viewProducts();
 
 // Menghapus produk dengan id
-console.log("\nMenghapus produk:");
 removeProduct(2);
 viewProducts();
